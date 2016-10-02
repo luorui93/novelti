09120438e8d08de949a81a36a14c1a6350c4b4e6
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-10-02 01:49:22 -0400

    vert tile and horiz tiles

--- lthmi_nav/src/node_map_divider.cpp
+++ lthmi_nav/src/node_map_divider.cpp
@@ -19,30 +19,29 @@
 */
 
 #include <lthmi_nav/map_divider.h>
-#include "map_divider_tile.cpp"
+#include "map_divider_vtile.cpp"
+#include "map_divider_htile.cpp"
 
 using namespace lthmi_nav;
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "map_divider");
     
-    //ros::NodeHandle n("~");
-    //std::string p;
-    //n.getParam("division_policy", p);
+    ros::NodeHandle n("~");
+    std::string p = "vtile";
+    n.getParam("division_policy", p);
     //VertTileMapDivider* mdiv = new VertTileMapDivider();
-    VertTileMapDivider mdiv;
+    /*HorizTileMapDivider mdiv;
     mdiv.run();
-    return 0;//mdiv.run1();
-//    if      (p=="vert_tiles")  return VertTileMapDivider().run();
-//     else if (p=="horiz_tiles") mdiv = Tile2by2MapDividerNode(false);
+    return 0;*/
+    if      (p=="vtile")  { VertTileMapDivider mdiv;  return mdiv.run(); }
+    else if (p=="htile")  { HorizTileMapDivider mdiv; return mdiv.run(); }
 //     else if (p=="extremals")   mdiv = ExtremalsMapDividerNode();
 //     else if (p=="equidists")   mdiv = EquiDist2MapDividerNode();
 //     else if (p=="mixed1")      mdiv = Mixed1MapDividerNode();
 //     else if (p=="mixed2")      mdiv = Mixed2MapDividerNode();
-//     else { 
-//         ROS_ERROR_NAMED(getNamespace(), "wrong value for 'division_policy' parameter, will die now"); 
-//         return 1;
-//     }
-    
-//     return mdiv.run();
+     else { 
+         ROS_ERROR("%s: wrong value for 'division_policy' parameter, will die now", getName().c_str());
+         return 1;
+     }
 }
