f47c76af14c4dcd402b046c957cf066af4f891e8
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-10-25 17:16:55 -0400

    clean up

--- lthmi_nav/nodes/node_param_publisher.py
+++ lthmi_nav/nodes/node_param_publisher.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python
-import os
 import rospy
-import subprocess
-
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
 
 """
 This is a helper node that allows to save ros parameters in rosbag-files.
 It waits for rosbag to start, then it reads all parameters, 
 serizlizes them into a string, and publishes as a String message 
 to topic /parameters.
+Example usage in laucnh file:
+
+    <node pkg="my_pkg" type="node_param_publisher.py" name="node_param_publisher">
+        <param name="rosbag_node_name" value="rosbag_record" />
+        <!-- any additional values from launch files you want to keep (such as remapped parameters (by default not save on param server) -->
+        <param name="prm1" value="$(arg prm1)" />
+        <param name="prm2" value="$(arg prm2)" />
+    </node>
+
 """
 
-
 if __name__=="__main__":
-    global pub
-    rospy.init_node('node_param_publisher.py')
-    rosbag_node_name = rospy.get_param('~rosbag_node_name', None)
-    if rosbag_node_name is None:
-        rospy.logerr("parameter rosbag_node_name has to be specified");
-        exit(1)
-    srv_path =rosbag_node_name+'/get_loggers'
-    rospy.loginfo("%s: waiting for service %s" % (rospy.get_name(), srv_path))
+    rospy.init_node('node_param_publisher')
+    rosbag_node_name = rospy.get_param('~rosbag_node_name', "/record")
+    srv_path = rosbag_node_name+'/get_loggers'
+    rospy.loginfo("%s: waiting for service %s (=waiting for rosbag to start)" % (rospy.get_name(), srv_path))
     rospy.wait_for_service(srv_path)
-    pub = rospy.Publisher('/parameters', String, queue_size=10, latch=True)
-    #directory = os.path.dirname(os.path.realpath(__file__))
-    #os.chdir(directory)
-    #rospy.set_param('git_commit', subprocess.check_output(['git', 'log', '--format="%H"', '-n', '1']))
-    #rospy.set_param('git_log', subprocess.check_output(['git', 'log', '-n', '1']))
+    pub = rospy.Publisher('/parameters', String, queue_size=1, latch=True)
     prms = rospy.get_param('/')
     pub.publish(str(prms))
     rospy.spin()
