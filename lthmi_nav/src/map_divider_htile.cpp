34fe65f30643466a160f1c2945394c71631be1b8
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-10-28 16:15:44 -0400

    Made tiels actual tiles

--- lthmi_nav/src/map_divider_htile.cpp
+++ lthmi_nav/src/map_divider_htile.cpp
@@ -1,87 +1,29 @@
-        /* +------+-------------+
-         * |           |        | 
-         * |     0     |    1   |
-         * |------+----+--------|
-         * |      |             |
-         * |  2   |      3      |
-         * |      |             |
-         * +------+-------------+  */
-        
+        /* +--------------------+
+         * |00000000000000000000|
+         * |00001111111111111111|
+         * |11111111111111111111|
+         * |11111111112222222222|
+         * |22222222222222222222|
+         * |22222222222222222233|
+         * +--------------------+  */
+
 #include <lthmi_nav/map_divider.h>
 
 namespace lthmi_nav {
 
-class HorizTileMapDivider :  public MapDivider {
+class VertTileMapDivider :  public MapDivider {
     public:
         int half;
 
-        HorizTileMapDivider() :
+        VertTileMapDivider() :
             MapDivider() 
         { }
         
         void divide() {
-            measure();
-            actuallyDivide();
-        }
-        
-        void measure() {
-            half = 0;
-            double halfProb = probs_optimal[0] + probs_optimal[1];
-            double prob = 0.0;
-            double p;
-            for (int y=0; y<pdf->info.height;y++) {
-                for (int x=0; x<pdf->info.width;x++) {
-                    p = pdf->data[y*pdf->info.width+x];
-                    if (p>0.0) {
-                        prob += p;
-                        if (prob > halfProb) {
-                            half = y;
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-            
-        void actuallyDivide() {
-            double prob=0;
-            int idx, cur_region = 0;
-            double p;
-            for (int x=0; x<pdf->info.width;x++) {
-                for (int y=0; y<=half;y++)  {
-                    idx = x + y*(pdf->info.width);
-                    p = pdf->data[idx];
-                    if (p>=0.0) {
-                        map_divided.data[idx] = cur_region;
-                        prob += p;
-                        if (cur_region==0 && prob >= probs_optimal[cur_region]) {
-                            probs_actual[cur_region] = prob;
-                            prob = 0.0;
-                            cur_region++;
-                        }
-                    }
-                }
-            }
-            probs_actual[cur_region] = prob;
-            prob = 0.0;
-            cur_region++;
-
-            for (int x=0; x<pdf->info.width;x++) {
-                for (int y=half+1; y<pdf->info.height;y++)  {
-                    idx = x + y*(pdf->info.width);
-                    p = pdf->data[idx];
-                    if (p>=0.0) {
-                        map_divided.data[idx] = cur_region;
-                        prob += p;
-                        if (cur_region==2 && prob >= probs_optimal[cur_region]) {
-                            probs_actual[cur_region] = prob;
-                            prob = 0.0;
-                            cur_region++;
-                        }
-                    }
-                }
-            }
-            probs_actual[cur_region] = prob;
+            for (int y=0; y<pdf->info.height;y++)
+                for (int x=0; x<pdf->info.width;x++)
+                    markVertex(x,y);
         }
 };
-}
+
+} //namespace lthmi_nav
