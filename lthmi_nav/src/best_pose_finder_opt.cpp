fe4c6b0e806c34fef4abf0e2419b30bca257b80d
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-11-04 23:52:28 -0400

    fixed found a clear bug when checking neighbours, added a bit more logging

--- lthmi_nav/src/best_pose_finder_opt.cpp
+++ lthmi_nav/src/best_pose_finder_opt.cpp
@@ -36,7 +36,7 @@ public:
         return meanDist;
     }
     
-    bool getMeanDist(Point point, double* dist) {
+    bool getMeanDist(Point point, double& dist) {
         // input (point) wrt to reach area
         // returns true if the mean distance has not been previously calculated for point (= reach_area had REACH_AREA_UNASSIGNED value for vertex point)
         int k = point.x + point.y*reach_area.info.width;
@@ -45,7 +45,7 @@ public:
             n_unassigned--;
             reach_area.data[k] = calcAvgDist(point);
         }
-        *dist = reach_area.data[k];
+        dist = reach_area.data[k];
         return was_unassigned;
     }
     
@@ -53,19 +53,30 @@ public:
         /*  moves p to local minimum
             * returns mean distance in optimal pose */
         double dist, min_dist;
-        getMeanDist(pt, &min_dist);
+        getMeanDist(pt, min_dist);
+        ROS_INFO("%s: Started slideToLocalMin at pt=(%d,%d), avgdist=%f, r2a=(%d,%d)", getName().c_str(), pt.x, pt.y, min_dist, r2a.x, r2a.y);
         int best_nb;
         while (true) {
             best_nb = -1; //best neighbour
+            #ifdef DEBUG_POSE_FINDER
+                ROS_INFO("%s: looking for best neighbour around pt=(%d,%d)", getName().c_str(), pt.x, pt.y);
+            #endif
             for (int nb=0; nb<8; nb++) { //iterate over neighbours
                 pt.x += NEIGHBOURS[nb][0];  pt.y += NEIGHBOURS[nb][1];
-                if (getMeanDist(pt, &dist) && dist>0 && dist < min_dist) {
+                if (getMeanDist(pt, dist) && dist>0 && dist < min_dist) {
                     min_dist = dist;
                     best_nb = nb;
                 }
+                #ifdef DEBUG_POSE_FINDER
+                    ROS_INFO("%s: tried pt=(%d,%d), dist=%f, min_dist=%f", getName().c_str(), pt.x, pt.y, dist, min_dist);
+                #endif
+                pt.x -= NEIGHBOURS[nb][0];  pt.y -= NEIGHBOURS[nb][1];
             }
             if (best_nb>=0) {
                 pt.x+=NEIGHBOURS[best_nb][0]; pt.y+=NEIGHBOURS[best_nb][1];
+                #ifdef DEBUG_POSE_FINDER
+                    ROS_INFO("%s: move to pt=(%d,%d)", getName().c_str(), pt.x, pt.y);
+                #endif
                 PUB_DEBUG_POSE(pt.x,pt.y, false);
             } else {
                 break;
