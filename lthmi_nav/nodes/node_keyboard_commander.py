45594001bd8bb01d8fbb6445b0728e421cbfd27a
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-12-18 20:39:11 -0500

    Added new_goal service call

--- lthmi_nav/nodes/node_keyboard_commander.py
+++ lthmi_nav/nodes/node_keyboard_commander.py
@@ -7,6 +7,7 @@ import sys, select, termios, tty
 from numpy import array, eye, random
 
 from lthmi_nav.msg import Command
+from std_srvs.srv import Empty
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -15,21 +16,35 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+
+def newGoal(new_goal_srv):
+    try:
+        resp = new_goal_srv()
+    except rospy.ServiceException, e:
+        rospy.logerr("Service call failed: %s"%e)
+        exit(1)
+    rospy.loginfo("%s: Initiating a new goal" % (rospy.get_name()))
+
+
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
     
-    pub = rospy.Publisher('/cmd_intended', Command, queue_size=10)
     rospy.init_node('keyboard_commander')
+    pub = rospy.Publisher('/cmd_intended', Command, queue_size=10)
+
+    srv_name = '/inference_unit/new_goal'
+    rospy.wait_for_service(srv_name)
+    new_goal_srv = rospy.ServiceProxy(srv_name, Empty)
     
     #read parameters
     key_mappings  = rospy.get_param('~key_mappings', ['0','1','2','3'])
+    new_goal_key  = rospy.get_param('~new_goal_key', 'r')
  
     key_mappings_dict ={}
     for index, key in enumerate(key_mappings):
         key_mappings_dict[key] = index
     
-    rospy.logwarn("=====KEY MAPPINGS:===== " + str(key_mappings_dict))
-    
+    rospy.logwarn("=====KEY MAPPINGS: %s, new goal command: '%s'"  % (str(key_mappings_dict), new_goal_key))
     
     try:
         while(1):
@@ -39,6 +54,8 @@ if __name__=="__main__":
                 msg = Command()
                 msg.cmd = intended
                 pub.publish(msg)
+            elif key==new_goal_key:
+                newGoal(new_goal_srv)
             else:
                 #x = 0
                 #th = 0
