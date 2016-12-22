f9716375b8d81f26698fb4be3f9e7ef8b21e4678
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-12-21 19:48:01 -0500

    Added a script to record vido from screen

---
+++ lthmi_nav/test/scripts/record_screen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+handler()
+{
+    echo quit | nc localhost 8082
+}
+
+trap handler SIGINT
+
+start_time=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
+
+vlc \
+screen:// \
+-I dummy  \
+--extraintf rc \
+--rc-host localhost:8082 \
+--no-video :screen-fps=24 :screen-caching=300 \
+--sout "#transcode{vcodec=h264,vb=8192,acodec=mp3,ab=192,fps=24,scale=1}:duplicate{dst=std{access=file,mux=mp4,dst='/home/sd/Videos/alden_hall_experiment_desktop_$start_time.mp4'}}"
+
+#--rc-quiet \
+#--dummy-quiet
+#--screen-follow-mouse \
+#--screen-mouse-image="mouse_pointer.png" \
+#--screen-left=0 --screen-top=0 --screen-width=800 --screen-height=600 \
