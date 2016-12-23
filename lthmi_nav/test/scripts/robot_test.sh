c2d555b02a3bed6fafc7bf65febe6b28a08dce12
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-12-23 15:14:19 -0500

    Added amazon comp id

--- lthmi_nav/test/scripts/robot_test.sh
+++ lthmi_nav/test/scripts/robot_test.sh
@@ -10,8 +10,8 @@ use_pois=0
 ksafe=1.3
 time_start=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
 
-#cmd="echo Echoing start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois"
-cmd="roslaunch lthmi_nav key.launch start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois ksafe:=$ksafe"
+cmd="echo Echoing start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois"
+#cmd="roslaunch lthmi_nav key.launch start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois ksafe:=$ksafe"
 
 time_end=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
 
@@ -21,6 +21,8 @@ machine_id=`cat /var/lib/dbus/machine-id`
 
 if [ "$machine_id" == "73fb7affe19b2f79f0bc720600000002"  ]; then
     machine="y580"
+elif [ "$machine_id" == "4faf6687b5689ea02e709d9800000002" ]; then
+    machine="amazon"
 else
     machine="other"
 fi
