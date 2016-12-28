ebcc8ab6a9e2ff6d3cba42f3caa5fff6537b8164
Dmitry Sinyukov <dmitry@sinyukov.com>
2016-12-28 15:32:18 -0500

    Added view_sizes and smooth_rads as node ROS params, some fixes

--- lthmi_nav/test/scripts/robot_test.sh
+++ lthmi_nav/test/scripts/robot_test.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-intent=debugging    #experiment
-start=door1
-dst=music1
+intent="test before recording"    #experiment
+start=door1     #door1   livroom1   office1   bathroom1  music1   bedroom1   door2
+dst=livroom1
 pos=nearcog_obst
 div=nearcog_extremal
 mx=mx85
 use_pois=0
 ksafe=1.3
-smoothen=true
+smooth_rads="[]"
+view_sizes="[16,32,64,128,256]"
 iarea_k=0.9
+
+
 time_start=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
 
 #cmd="echo Echoing start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois"
-cmd="roslaunch lthmi_nav key.launch start:=$start dst:=$dst pos:=$pos div:=$div use_pois:=$use_pois ksafe:=$ksafe mx:=$mx smoothen:=$smoothen iarea_k:=$iarea_k"
+cmd="roslaunch lthmi_nav key.launch  start:=$start  dst:=$dst  pos:=$pos  div:=$div  use_pois:=$use_pois  ksafe:=$ksafe  mx:=$mx  view_sizes:=\"$view_sizes\"  smooth_rads:=\"$smooth_rads\"  iarea_k:=$iarea_k"
 
-time_end=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
-
+echo "Running: '$cmd'"
 $cmd
 
+time_end=`date +"%Y-%m-%d_%H-%M-%S_%Z%z"`
 machine_id=`cat /var/lib/dbus/machine-id`
-
 if [ "$machine_id" == "73fb7affe19b2f79f0bc720600000002"  ]; then
     machine="y580"
 elif [ "$machine_id" == "4faf6687b5689ea02e709d9800000002" ]; then
@@ -28,6 +30,7 @@ elif [ "$machine_id" == "4faf6687b5689ea02e709d9800000002" ]; then
 else
     machine="other"
 fi
+
 
 {
     echo "run_intent:   $intent"
@@ -40,8 +43,11 @@ fi
     echo "ksafe:        $ksafe"
     echo "intrstarea_k: $iarea_k"
     echo "machine:      $machine"
+    echo "smooth_rads:  $smooth_rads"
+    echo "view_sizes:   $view_sizes"
     echo "time_start:   '$time_start'"
     echo "time_end      '$time_end'"
+    echo "command:      '$cmd'"
     echo "result:       '???'"
     echo 
     echo
