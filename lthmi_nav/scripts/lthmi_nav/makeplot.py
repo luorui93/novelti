4d2075307e8d6e56b605fd8b878e650ec1787590
Dmitry Sinyukov <dmitry@sinyukov.com>
2017-01-28 18:58:49 -0500

    some improvements for table visualisations

--- lthmi_nav/scripts/lthmi_nav/makeplot.py
+++ lthmi_nav/scripts/lthmi_nav/makeplot.py
@@ -10,6 +10,11 @@ usage = """ USAGE:
 
 """
 
+"""
+./makeplot.py ~/Desktop/lthmi_nav_data/sim/amazon/stats/1/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/2/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/3/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/4/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/5/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/6/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/7/*.txt ~/Desktop/lthmi_nav_data/sim/amazon/stats/8/*.txt 
+"""
+
+
 def fff(self, d):
     print d['t_div']
     print d['t_nav']
@@ -24,25 +29,51 @@ if __name__=="__main__":
         print usage
         sys.exit(1)
         
+
+        
     files = sys.argv[1:]
     #print files
     dt = DataTable.fromCsvFiles(files, 
         filter_dict = {
-            'pos': lambda method: method in ["no_move", "ra_maxprob", "maxprob_obst", "nearcog_obst", "cog2lopt", "ramaxprob2lopt", "maxprob2lopt"]
+            #'pos': lambda method: method in ["no_move", "ra_maxprob", "maxprob_obst", "nearcog_obst", "cog2lopt", "ramaxprob2lopt", "maxprob2lopt", "gopt"]
+            'pos': lambda method: method in ["no_move", "ra_maxprob", "maxprob_obst", "nearcog_obst", "cog2lopt"]
+            
         },
         calculated_values = {
-            't_div_': lambda self, d: d['t_div']/d['t_nav'],
-            't_pos_': lambda self, d: d['t_pos']/d['t_nav'],
-            't_pdf_': lambda self, d: d['t_pdf']/d['t_nav'],
+            #'t_div_': lambda self, d: d['t_div']/d['t_nav'],
+            #'t_pos_': lambda self, d: d['t_pos']/d['t_nav'],
+            #'t_pdf_': lambda self, d: d['t_pdf']/d['t_nav'],
             't_nav_corr': lambda self, d: d['t_nav']-d['t_pos']-d['t_div'],
         }
     )
-    #print "======================================================"
-    #print dt.data['t_pos
-    #print "-------------------------------------------------"
-    dims = ('mx', '_res_', 'pos','div','path') # 5 dimensions
-    vals = ('t_pdf_', 't_pos_', 't_div_', 't_nav_corr')#, 'over_len', 'sep2nav', 'over_time')
-    #vals = ('sep2nav',)
-    v = Table2NDimVector(dt, dims, vals)
-    v.display4dPage('mx91')
+    print "Total number of lines in the data table = %d"  % dt.getNumberOfRecords()
+    print "All matrices: %s" % (str(dt.getUniqueRecordsInColumn("mx")))
+    print "All velocities: %s" % (str(dt.getUniqueRecordsInColumn("vel")))
+    print "All periods: %s" % (str(dt.getUniqueRecordsInColumn("period")))
+    print "All divs: %s" % (str(dt.getUniqueRecordsInColumn("div")))
+    v = Table2NDimVector(dt, 
+        dim_names   = ('mx', '_res_', 'pos','path','div'),  # 5 dimensions 
+        result_cols = ('over_len', 't_nav', 't_nav_corr') # 'vel', 'period', 'sep2nav', 'over_time'
+    )
+    v.display4dPage('mx70',
+        renames = {
+            "plot_group_names":  {100: "route 1", 103: "route 2", 110: "route 3"},
+            "page_row_names": {'over_len': 'shortest dist/drive dist', 't_nav':'navigation duration, sec', 't_nav_corr':'navigation duration, sec\n (calculation time excluded)'},
+            "page_col_names": {
+                "no_move"       : "no move until inferred", 
+                "ra_maxprob"    : "max prob in reach area", 
+                "maxprob_obst"  : "closest to max prob", 
+                "nearcog_obst"  : "closest to COG", 
+                "cog2lopt"      : "local optimum"
+            },
+            "plot_color_names": {
+                'htile'     : "horizontal tile",
+                'vtile'     : "vertical tile", 
+                'altertile' : "alternating tile",
+                'extremal'  : "by extremals",
+                'equidist'  : "by equdistants",
+                "extredist" : "extremals and equidistants",
+            }
+        }
+    )
 
