c093427965a94be8a0f122c4c61cfabf2cc7801c
Dmitry A. Sinyukov <dmitry@sinyukov.com>
2018-10-24 21:32:59 -0400

    new orientation control that implements inference unit interface

--- novelti/include/novelti/orientation_control.h
+++ novelti/include/novelti/orientation_control.h
@@ -1,58 +1,73 @@
-#ifndef ORIENTATION_CONTROL_H
-#define ORIENTATION_CONTROL_H
+#ifndef NOVELTI_ORIENTATION_CONTROL_H
+#define NOVELTI_ORIENTATION_CONTROL_H
+
+#include <vector>
+#include <string>
 
 #include <ros/ros.h>
+#include <std_srvs/Empty.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
+
 #include <novelti/IntMap.h>
+#include <novelti/FloatMap.h>
 #include <novelti/OrientationPdf.h>
+#include <novelti/StartExperiment.h>
 #include <novelti/Command.h>
-#include <vector>
+#include <novelti/common.cpp>
+#include <novelti/inference_unit.h>
+#include <novelti/position_control.h>
+#include <novelti/orientation_disk_divider.h>
+#include <novelti/opt_orientation_selector.h>
 
 namespace novelti {
 
-struct Arc {
-    float lower_angle;
-    float upper_angle;
-    int color;
-};
 
-struct EndPoint {
-    int x;
-    int y;
-    int quadrant;
-};
-
-class OrientationControl {
+class OrientationControl : public InferenceUnit {
 public:
-
-    OrientationControl();
-    void initDisplay(OrientationPdfConstPtr opdf);
-    void orientationPdfCallback(OrientationPdfConstPtr pdf);
-    void setColor(int x,int y,int color);
-    void updateOptimalPdf(int cur_color);
-    void markUnitColor();
-    void updateArc();
-    EndPoint calculateEndPoint(float);
-    void divideAndPublish();
-    void drawSector();
-    void resetArc();
-    void highlightSelection(novelti::CommandConstPtr msg);
-    void resetCanvas();
+    const float PDF_UNREACHABLE = -10.0;
     
-    ros::NodeHandle node;
-    ros::Publisher pub_canvas,pub_selection_highlight;
+    enum State { RELAXING, INFERRING_ORIENTATION, INFERRED};
+    State state;
+    
+    ros::NodeHandle&    node_;
+    ros::Publisher      pub_opdf_;
+    ros::Publisher      pub_pose_inf_;
+    
+    OrientationPdf  opdf;
+    std::vector<int> orientation_divided;
+    geometry_msgs::PoseStamped pose_inferred;
+    
+    std::vector<double> interface_matrix;
+    std::vector<double> priors;
+    std::vector<double> posteriors;
+    std::vector<double> coefs;
+    
+    float uniform_prob_;
+    float eps_;
+    double max_oprob_;
+    int max_oprob_k_;
+    float orientation_resol_;
+    float orientation_inferred_;
+    DiskDivider* disk_div_;
+    OptOrientationSelector* orien_sel_;
+    OrientationPdf opdf_;
+    const PositionControl& pc_; 
+    
+    OrientationControl(ros::NodeHandle& node, const PositionControl& position_control);
+    //InferenceUnit(const std::string paramPrefix);
+    void start(novelti::StartExperiment::Request& req);
+    void stop();
+    const std::vector<int>& getOrientationColor();
+    void initPriors(std::vector<double>& new_priors);
+    void update(const std::vector<double>& coefs, const int cmd, std::vector<double>& priors);
+    void onInferred(int inferredCmd);
+    void act();
+    void pubOpdf();
 
-    IntMap canvas,selection_highlight,transparent_canvas;
-    novelti::OrientationPdfConstPtr opdf_ptr;
-    double radius;
-    int n_cmd;
-    int color_order;
-    double resol,x_center,y_center;
-
-    std::vector<float> cur_color_pdf;
-    std::vector<float> optimal_color_pdf;
-    std::vector<int> unit_color;
-    std::vector<Arc> arc_vector;
+    
 };
-
 }
+
 #endif
--- novelti/src/orientation_control.cpp
+++ novelti/src/orientation_control.cpp
@@ -1,155 +1,84 @@
 #include <novelti/orientation_control.h>
-#include "SectorWalker.hpp"
-#include <math.h>
 
 using namespace novelti;
 
-OrientationControl::OrientationControl():
-    node("~") 
+OrientationControl::OrientationControl(ros::NodeHandle& node, const PositionControl& position_control) :
+    node_(node),
+    pc_(position_control)
 {
-    node.param("ori/compass_radius", radius, 100.0);
-    node.param("ori/command_number", n_cmd, 4);
-    node.param("ori/orientation_resolution", resol, 5.0);
+    node_.param<float>("ori/orientation_resolution", orientation_resol_, 5.0);
+    node_.param<float>("inf/eps", eps_, std::numeric_limits<float>::epsilon());
 
-} 
-
-void OrientationControl::initDisplay(OrientationPdfConstPtr opdf) {
-    canvas = IntMap();
-    canvas.header.frame_id = "/map";
-    canvas.info.width = 2*radius + 3;
-    canvas.info.height = 2*radius + 3;
-    canvas.info.resolution = 0.1;
-    canvas.info.origin.position.x = 0.0;
-    canvas.info.origin.position.y = 0.0;
-    canvas.data = std::vector<int>(canvas.info.width*canvas.info.height, 255);
-    pub_canvas = node.advertise<novelti::IntMap>("/orientation_divided", 1, true);
-    pub_selection_highlight = node.advertise<novelti::IntMap>("/highlight_orientation",1,false);
-    selection_highlight = canvas;
-    transparent_canvas = canvas;
-
-    unit_color = std::vector<int>(opdf->data.size(),-1);
-    arc_vector = std::vector<novelti::Arc>(n_cmd);
-
-    optimal_color_pdf = std::vector<float>(n_cmd,1.0/n_cmd);
-    cur_color_pdf = std::vector<float>(optimal_color_pdf.size(),0.0);
-
-    x_center = radius;
-    y_center = radius;
-
-    opdf_ptr = opdf;
-    markUnitColor();
-    updateArc();
-    drawSector();
-}   
-
-void OrientationControl::resetCanvas() {
-    pub_canvas.publish(transparent_canvas);
+    disk_div_ = new DiskDivider();
+    orien_sel_ = new OptOrientationSelector();
 }
 
-void OrientationControl::orientationPdfCallback(OrientationPdfConstPtr opdf) {
-    opdf_ptr = opdf;
-    divideAndPublish();
-}   
-
-void OrientationControl::highlightSelection(novelti::CommandConstPtr msg){
-    int cmd = msg->cmd;
-
-    selection_highlight.data = canvas.data;
-    for (std::vector<int>::iterator it=selection_highlight.data.begin(); it != selection_highlight.data.end(); it++) {
-        if (*it == cmd)
-            continue;
-        else *it = 255;
-    }
-    pub_selection_highlight.publish(selection_highlight);
-    usleep(0.2*1000000);
-    pub_selection_highlight.publish(transparent_canvas);
+OrientationPdfConstPtr orientationPdfToPtr (OrientationPdf pdf) {
+    OrientationPdfConstPtr ptr(new OrientationPdf(pdf));
+    return ptr;
 }
 
-void OrientationControl::setColor(int x, int y, int color) {
-    canvas.data[x + y*canvas.info.width] = color;
+void OrientationControl::start(novelti::StartExperiment::Request& req) {
+    opdf_ = OrientationPdf();
+    opdf_.header.frame_id = "/orientation_display";
+    opdf_.data = std::vector<float>(360 / orientation_resol_,0.0);
+
+    pub_pose_inf_ = node_.advertise<geometry_msgs::PoseStamped>("/pose_inferred", 1, true);
+    pub_opdf_     = node_.advertise<OrientationPdf>("/opdf", 1, true);
+
+    disk_div_->initDisplay(orientationPdfToPtr(opdf_));
+    ROS_INFO("Orientation Control Started...");
+    orien_sel_->start();
+    ROS_INFO("Opt Orientation Selector Started...");
 }
-void OrientationControl::updateOptimalPdf(int cur_color) {  //Try to divide region as balanced as possible based on optimal pdf
-    float total_prob = 0.0;
-    for (int i=0;i<=cur_color;i++) {
-        total_prob += optimal_color_pdf[i];
-    }
-    //ROS_WARN("total_prob: %f",total_prob);
-    for (int i=cur_color+1;i < optimal_color_pdf.size();i++) {
-        optimal_color_pdf[i] = (1-total_prob) / (optimal_color_pdf.size()-cur_color-1);
-    }
-    //ROS_WARN("optimal_color_pdf: [%f,%f,%f,%f]",optimal_color_pdf[0],optimal_color_pdf[1],optimal_color_pdf[2],optimal_color_pdf[3]);
+
+void OrientationControl::stop() {
+    pub_pose_inf_.shutdown();
+    pub_opdf_.shutdown();
+}
+
+const std::vector<int>& OrientationControl::getOrientationColor() {
+    return disk_div_->unit_color;
+}
+
+void OrientationControl::initPriors(std::vector<double>& new_priors) {
+    float uniform_prob = 1.0 / opdf_.data.size(); 
+    PdfUtils::setUniform(opdf_.data, uniform_prob_);
+    pubOpdf();
+    disk_div_->orientationPdfCallback(orientationPdfToPtr(opdf_));
+
+    //Calculate new prioris based on current opdf
+    PdfUtils::accumulate<float>(opdf_.data, getOrientationColor(), new_priors);
 
 }
 
-void OrientationControl::markUnitColor() {   //update color for minimal unit (resolution)
-    int cur_color = 0;
-    std::fill(cur_color_pdf.begin(),cur_color_pdf.end(),0.0);
-    for (int i=0; i<opdf_ptr->data.size();i++) {
-        cur_color_pdf[cur_color] += opdf_ptr->data[i];
-        if (cur_color_pdf[cur_color] > optimal_color_pdf[cur_color] + std::numeric_limits<float>::epsilon() && cur_color < n_cmd - 1) {  //TODO: a better way to add epsilon
+void OrientationControl::update(const std::vector<double>& coefs, const int cmd, std::vector<double>& priors) {
+    disk_div_->highlightSelection(cmd);
+    PdfUtils::update<float>(opdf.data, getOrientationColor(),coefs);
+    PdfUtils::denullifyNormalize<float>(opdf.data, eps_);
 
-            cur_color_pdf[cur_color] -= opdf_ptr->data[i];
-            updateOptimalPdf(cur_color);
-            //ROS_WARN("turning point: %d",i);
-            cur_color++;
-            cur_color_pdf[cur_color] += opdf_ptr->data[i];
-        }
-        
-        unit_color[i] = cur_color;
-    }   
+    PdfUtils::accumulate<float>(opdf.data, getOrientationColor(), priors);
 }
 
-void OrientationControl::resetArc() {
-    for (int i=0;i<arc_vector.size();i++) {
-        arc_vector[i].color = 0;
-        arc_vector[i].upper_angle = 0.0;
-        arc_vector[i].lower_angle = 0.0;
-    }
+void OrientationControl::onInferred(int inferredCmd) {
+    orientation_inferred_ = max_oprob_k_ * orientation_resol_ * M_PI / 180;
+    ROS_WARN("Orientation Inferred: %f", orientation_inferred_);
+    pose_inferred = pc_.getPositionInferred();
+    pose_inferred.pose.orientation = tf::createQuaternionMsgFromYaw(orientation_inferred_);
+    pub_pose_inf_.publish(pose_inferred);
 }
 
-void OrientationControl::updateArc() {   //update arc color
-    int cur_color = 0;
-    resetArc();
-    for (int i=0;i<unit_color.size();i++) {
-        if (unit_color[i] == cur_color) {
-            arc_vector[cur_color].color = cur_color;
-            arc_vector[cur_color].upper_angle += resol * M_PI/180;
-        }
-        else if (cur_color < n_cmd - 1) {
-            arc_vector[cur_color+1].lower_angle = arc_vector[cur_color].upper_angle;
-            arc_vector[cur_color+1].upper_angle = arc_vector[cur_color+1].lower_angle + resol*M_PI/180;
-            cur_color++;
-            arc_vector[cur_color].color = cur_color;
-        }
-    }
-    // for (int i=0;i<arc_vector.size();i++) {
-    //     ROS_WARN("arc_vector:%f,%f,%d",arc_vector[i].upper_angle,arc_vector[i].lower_angle,i);
-    // }
+void OrientationControl::act() {
+    pubOpdf();
+    
+    orien_sel_->findOptOrientation(orientationPdfToPtr(opdf));
+    disk_div_->orientationPdfCallback(orientationPdfToPtr(opdf));
 }
 
-EndPoint OrientationControl::calculateEndPoint(float angle) {   //calculate arc endpoint
-    EndPoint p;
-    p.quadrant = floor(2*angle / M_PI);
-    angle = angle - p.quadrant * M_PI/2;
-    p.x = floor(radius * cos(angle));
-    p.y = floor(radius * sin(angle));
-}
-
-void OrientationControl::drawSector() {
-    std::fill(canvas.data.begin(),canvas.data.end(),255);
-    for (int i=0; i < arc_vector.size();i++) {
-        if (!(arc_vector[i].lower_angle == arc_vector[i].upper_angle) ){
-        // ROS_WARN("----------------------Paint sector %d", i);
-            for (auto pt: cwave::SectorPoints(cwave::Point(x_center,y_center), arc_vector[i].lower_angle, arc_vector[i].upper_angle, radius,10000)) {
-                setColor(pt.x,pt.y,arc_vector[i].color);
-            }    
-        }
-    }
-}
-
-void OrientationControl::divideAndPublish() {
-    markUnitColor();
-    updateArc();
-    drawSector();
-    pub_canvas.publish(canvas);
+void OrientationControl::pubOpdf() {
+    opdf_.header.stamp = ros::Time::now();
+    pub_opdf_.publish(opdf);
+    ros::spinOnce();
+    //ROS_INFO("%s: =========== published opdf (SEQ=%d), max_oprob=%f", getName().c_str(), opdf.header.seq, max_oprob);
+    opdf_.header.seq++;
 }
